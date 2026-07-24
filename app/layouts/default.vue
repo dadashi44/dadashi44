5ee8404eca6d8c5729b5c0ffeb023465b80192b6
asianParadissseee <asian.paradissseee@gmail.com>
2026-07-24 22:07:13 +0500

    feat(layout): default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="grain relative flex min-h-svh flex-col">
+    <ScrollProgress />
+    <TheHeader />
+    <TheMenu />
+    <main class="flex-1 pt-16 lg:pt-20">
+      <slot />
+    </main>
+    <TheFooter />
+  </div>
+</template>
