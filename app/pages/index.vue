b0d342cd0ad61ff423e2f03ff57ae1864713b321
asianParadissseee <asian.paradissseee@gmail.com>
2026-08-03 20:07:54 +0500

    feat(pages): home

---
+++ app/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const {t} = useI18n()
+
+useSiteSeo({
+  title: () => t('home.seoTitle'),
+  description: () => t('home.seoDescription'),
+  type: 'profile'
+})
+</script>
+
+<template>
+  <div>
+    <HeroSection />
+    <IntroSection />
+    <ServicesSection />
+    <WorksShowcase />
+    <StackSection />
+    <ContactCta />
+  </div>
+</template>
